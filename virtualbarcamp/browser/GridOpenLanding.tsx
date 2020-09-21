c28183cda8a3624edd5647dfc426956712b78d88
Chris Northwood <chris@pling.org.uk>
2020-09-21 23:52:56 +0100

    fix "Access Discord" button styling

--- virtualbarcamp/browser/GridOpenLanding.tsx
+++ virtualbarcamp/browser/GridOpenLanding.tsx
@@ -9,14 +9,15 @@ const GridOpenLanding: FunctionComponent = () => (
     <div className="hero">
       <div className="hero-body">
         <div className="container">
+          <h2 className="title">The Grid</h2>
           <div className="level">
             <div className="level-item level-left">
               <div>
-                <h2 className="title">The Grid</h2>
                 <p className="subtitle">
                   The grid shows the sessions that are running, at what time and in which room.
                   <br />
-                  Want to run your own session? Fill in the blank card and drag it into a free spot!
+                  Want to run your own session? Fill in the blank card below the grid and drag it
+                  into a free spot!
                 </p>
               </div>
             </div>
